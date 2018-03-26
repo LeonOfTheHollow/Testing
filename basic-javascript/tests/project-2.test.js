93bdbf62e60889108b1b76f6269c9a4dbf105aff
Mephestys <mephestys@gmail.com>
2018-03-26 16:08:46 -0600

    Added greeting, isTenOrFive, isInRange, isInteger, fizzBuzz tests - fizzBuzz tests needs better wording.

--- basic-javascript/tests/project-2.test.js
+++ basic-javascript/tests/project-2.test.js
@@ -23,12 +23,31 @@ describe('Project-2 Functions', () => {
     it('should be a function', () => {
       assert.typeOf(greeting, 'function');
     });
+    it('should return "Guten Tag!" if the language is German', () => {
+      assert.strictEqual(greeting('German'), 'Guten Tag!');
+    });
+    it('should return "Hola!" if the language is Spanish', () => {
+      assert.strictEqual(greeting('Spanish'), 'Hola!');
+    });
+    it('should return "Hello!" if no language is provided', () => {
+      assert.strictEqual(greeting(), 'Hello!');
+    });
   });
 
   describe('`isTenOrFive`', () => {
     const isTenOrFive = funcs.isTenOrFive;
     it('should be a function', () => {
       assert.typeOf(isTenOrFive, 'function');
+    });
+    it('should return a Boolean', () => {
+      assert.typeOf(isTenOrFive(10), 'Boolean');
+    });
+    it('should return true if a number is 10 or 5', () => {
+      assert.strictEqual(isTenOrFive(10), true);
+      assert.strictEqual(isTenOrFive(5), true);
+    });
+    it('should return false for a number that is not 10 or 5', () => {
+      assert.strictEqual(isTenOrFive(11), false);
     });
   });
 
@@ -37,6 +56,17 @@ describe('Project-2 Functions', () => {
     it('should be a function', () => {
       assert.typeOf(isInRange, 'function');
     });
+    it('should return a Boolean', () => {
+      assert.typeOf(isInRange(10), 'Boolean');
+    });
+    it('should return true if a number is between 20 and 50', () => {
+      assert.isTrue(isInRange(49), true);
+      assert.isTrue(isInRange(21), true);
+    });
+    it('should return false if a number is less than 20 or greater than 50', () => {
+      assert.isFalse(isInRange(19), false);
+      assert.isFalse(isInRange(51), false);
+    });
   });
 
   describe('`isInteger`', () => {
@@ -44,12 +74,33 @@ describe('Project-2 Functions', () => {
     it('should be a function', () => {
       assert.typeOf(isInteger, 'function');
     });
+    it('should return a Boolean', () => {
+      assert.typeOf(isInteger(10), 'Boolean');
+    });
+    it('should return true if the number is an integer', () => {
+      assert.isTrue(isInteger(5), true);
+    });
+    it('should return false if the number is not an integer', () => {
+      assert.isFalse(isInteger(7.25), false);
+    });
   });
 
   describe('`fizzBuzz`', () => {
     const fizzBuzz = funcs.fizzBuzz;
     it('should be a function', () => {
       assert.typeOf(fizzBuzz, 'function');
+    });
+    it('should return "fizzbuzz" for reasons', () => {
+      assert.strictEqual(fizzBuzz(30), 'fizzbuzz');
+    });
+    it('should return "buzz" for other reasons', () => {
+      assert.strictEqual(fizzBuzz(10), 'buzz');
+    });
+    it('should return "fizz" for even other reasons', () => {
+      assert.strictEqual(fizzBuzz(6), 'fizz');
+    });
+    it('should return the number if it can\'t fizzbuzz', () => {
+      assert.strictEqual(fizzBuzz(1), 1);
     });
   });
 
